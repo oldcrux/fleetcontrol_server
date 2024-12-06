8daa9ba54f766618376727a142d743a54dadc8c2
Rashmi Ranjan Parida <>
2024-12-05 22:40:20 -0500

    increase bulk upload size limit

--- src/app.ts
+++ src/app.ts
@@ -46,7 +46,7 @@ const numCPUs = os.cpus().length;
 // const bodyParser = require("body-parser");
 
 // app.use(bodyParser.json());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' })); 
 const corsOptions = {
   origin: true, // Replace with your frontend URL
   methods: ['GET', 'POST'], // Allowed methods
