a8636a2aa0db75d71dabb8ae4304dffb3bc39add
Rashmi Ranjan Parida <>
2024-12-29 21:28:30 -0500

    geofence touch status reset job schedule

--- src/route/BullmqRoute.ts
+++ src/route/BullmqRoute.ts
@@ -1,18 +1,21 @@
 import { Router } from 'express';
-import { deleteQueue, inspectMultipleQueues, inspectQueue, purgeQueue } from '../controller/BullmqController';
+import { deleteQueue, inspectAllQueues, inspectQueue, purgeAllQueues, purgeQueue } from '../controller/BullmqController';
 
 const router = Router();
 
 // Define routes
 
 // GET {url}/node/api/bull/queue/inspect/all
-router.get('/queue/inspect/all', inspectMultipleQueues);
+router.get('/queue/inspect/all', inspectAllQueues);
 
 // GET {url}/node/api/bull/queue/inspect
 router.get('/queue/inspect', inspectQueue);
 
 // POST {url}/node/api/bull/queue/purge
 router.post('/queue/purge', purgeQueue);
+
+// POST {url}/node/api/bull/queue/purge/all
+router.post('/queue/purge', purgeAllQueues);
 
 // POST {url}/node/api/bull/queue/delete
 router.post('/queue/delete', deleteQueue);
